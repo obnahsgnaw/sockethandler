dd4730181336b4a1e312ea7f30ce396fc0610723
wangsb <wangsb@efly-cetc.com>
2023-11-16 14:44:58 +0800

    perf(handler): added wss flb num support

--- sockettype/type.go
+++ sockettype/type.go
@@ -1,10 +1,38 @@
 package sockettype
 
+import "github.com/obnahsgnaw/application/servertype"
+
 // SocketType 服务类型
 type SocketType string
 
 func (s SocketType) String() string {
 	return string(s)
+}
+
+func (s SocketType) ToServerType() servertype.ServerType {
+	switch s {
+	case TCP, TCP4, TCP6:
+		return servertype.Tcp
+	case WSS:
+		return servertype.Wss
+	case UDP, UDP4, UDP6:
+		return servertype.Udp
+	default:
+		panic("trans socket type to server type failed")
+	}
+}
+
+func (s SocketType) ToHandlerType() servertype.ServerType {
+	switch s {
+	case TCP, TCP4, TCP6:
+		return servertype.TcpHdl
+	case WSS:
+		return servertype.WssHdl
+	case UDP, UDP4, UDP6:
+		return servertype.UdpHdl
+	default:
+		panic("trans socket type to server type failed")
+	}
 }
 
 // 服务类型定义
